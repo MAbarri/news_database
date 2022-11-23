0d44136302db503c56c24f7f977be88c1c611297
Mohamed Abarri <officiel.abarri@gmail.com>
2022-11-23 13:40:17 +0100

    minor fix

--- src/news-retreiver/news-retreiver.service.ts
+++ src/news-retreiver/news-retreiver.service.ts
@@ -6,9 +6,9 @@ import { ArticleService } from "../api/article/article.service"
 export class NewsRetreiverService {
     constructor(private articleService: ArticleService){}
     
-    @Cron('1 0 * * * *')
+    @Cron('1 * * * * *')
     async handleCron() {
-        console.log('Called when the current second is 45');
+        console.log('Called when the current second is 1 at minute 0');
         await this.articleService.scrapNewsData("sa", "general")
         await this.articleService.scrapNewsData("sa", "business")
         await this.articleService.scrapNewsData("sa", "sports")
